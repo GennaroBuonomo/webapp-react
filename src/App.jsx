f477018ac2063d6499500e0ce6d4372e8e143491
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-19 18:09:06 +0200

    Importazione delle pagine HomePage e DetailMovie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import DetailMovie from "./pages/DetailMovie";
-import { BrowserRouter, Routers, Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return 
+  return (
+    <BrowserRouter>
+     <Routes>
+       <Route element={<DefaultLayout />}>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/movies/:id" element={<DetailMovie />} />
+       </Route>
+     </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
