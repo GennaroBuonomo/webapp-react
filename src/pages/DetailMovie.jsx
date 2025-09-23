86d3c85ead464f7a8ce0e6a68e21785824848357
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-23 23:24:18 +0200

    Inizio e fine pagina dettaglio con le recensioni

--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const DetailMovie = () => {
+  //Recupero l'id passato alla rotta
+  const { id } = useParams();
+
+  //Definisco la variabile di stato
+  const [movie, setMovie] = useState({})
+
+  //Definisco il metodo che mi effettua la chiamata ajax per recuperare il libro attraverso l'id
+  const fetchMovie = () => {
+    axios.get(`http://localhost:3000/api/movies/${id}`).then((resp) => {
+      setMovie(resp.data);
+    })
+    .catch((err) => console.log(err));
+  };
+
+  useEffect(fetchMovie, [])
+
   return (
-    <div>
-      Dettaglio dei film
+   <div className="container">
+    <div className="row">
+      <div className="col-12">
+        <div className="detail-card">
+          <div className="d-flex">
+            <div className="cover-image">
+              <img src={movie.image} alt={movie.title} />
+            </div>
+            <div className="text-details">
+              <h1 className="title">{movie.title}</h1>
+              <p className="release-year">{movie.release_year}</p>
+              <p className="genre">{movie.genre}</p>
+              <h3 className="director"><em>Diretto da: {movie.director}</em></h3>
+              <p className="abstract">{movie.abstract}</p>
+            </div>
+          </div>
+        </div>
+        <div className="reviews">
+          {movie.reviews && movie.reviews.map((review, index) => {
+            return(
+              <div className="review-card mb-4" key={index}>
+               <strong>{review.name}</strong> - Voto: {review.vote}<br />
+               <em>{review.text}</em>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
+   </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,51 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return (
+  //Definisco le variabili di stato
+  const [movies, setMovies] = useState([]);
+
+  //funzione che recupera i film con la chiamata ajax
+  const fetchMovies = () => {
+    axios.get("http://localhost:3000/api/movies").then((resp) => {
+      setMovies(resp.data)
+    })
+  };
+
+  //Utilizzo useEffect per recuperare la lista dei film
+  useEffect(fetchMovies, []);
+
+    return (
     <div className="container my-5">
       <div className="row">
-        <div className="col-12 text-center">
+        <div className="col-12 text-center mb-3">
           <h1>BoolMovies</h1>
           <h2>
             <i>Film per i veri appassionati</i>
           </h2>
         </div>
+      </div>
         <div className="row gy-3">
-          <div className="col-12 col-md-6 col-lg-4">
-            <div className="card-movie">
-              <img src="./imgs/spiderman.jpeg" className="cover-movie-card" alt="film" />
-              <div className="overlay">
-                <h1>Titolo del film</h1>
-                <p>direttore delle riprese</p>
+          {movies.map((movie) => {
+            const { id, image, title, director } = movie;
+            return(
+              <div className="col-12 col-md-6 col-lg-4" key={id}>
+                <Link to={`/movies/${id}`}>
+                 <div className="card-movie">
+                   <img src={image} className="cover-movie-card" alt={title} />
+                   <div className="overlay">
+                     <h1>{title}</h1>
+                     <p>{movie.director}</p>
+                   </div>
+                 </div>         
+                </Link>
               </div>
-            </div>
-          </div>
-        </div>
+            );
+          })}
       </div>
     </div>
-   )
-}
+   );
+};
 
 export default HomePage
